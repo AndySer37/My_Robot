2f4c844c55f01f92a7164554832465ed237c707d
AnduSer37 <andy19970307@gmail.com>
2019-05-05 01:16:42 +0800

    add

--- catkin_ws/src/ssd_mobile_lite/src/bb_ssd_mobile_lite.py
+++ catkin_ws/src/ssd_mobile_lite/src/bb_ssd_mobile_lite.py
@@ -100,18 +100,18 @@ class bb_ssd_mobile_lite(object):
 		time = rospy.get_time()
 		boxes, labels, probs = self.predictor.predict(image, 10, self.prob_threshold)
 		print(1./(rospy.get_time() - time))
-		for i in range(boxes.size(0)):
-			box = boxes[i, :]
-			check = True
-			for j in box:
-				if not 0 <= j < 640:
-					check = False
-			if check:
-				if (box[0], box[1]) != (box[2], box[3]):
-					cv2.rectangle(img, (box[0], box[1]), (box[2], box[3]), (255, 255, 0), 4)
+		# for i in range(boxes.size(0)):
+		# 	box = boxes[i, :]
+		# 	check = True
+		# 	for j in box:
+		# 		if not 0 <= j < 640:
+		# 			check = False
+		# 	if check:
+		# 		if (box[0], box[1]) != (box[2], box[3]):
+		# 			cv2.rectangle(img, (box[0], box[1]), (box[2], box[3]), (255, 255, 0), 4)
 
-					label = "{}: {:.2f}".format(self.labels[labels[i]], probs[i])
-					cv2.putText(img, label,(box[0] + 20, box[1] + 40),cv2.FONT_HERSHEY_SIMPLEX,1,(255, 0, 255),2)
+		# 			label = "{}: {:.2f}".format(self.labels[labels[i]], probs[i])
+		# 			cv2.putText(img, label,(box[0] + 20, box[1] + 40),cv2.FONT_HERSHEY_SIMPLEX,1,(255, 0, 255),2)
 
 		return img# , objs
 
